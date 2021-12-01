718052db833dbc3c044ad89d659ee97718ad77fd
expiredy101 <yartaoley2005@mail.ru>
2021-12-01 18:23:00 +0300

    Stating drawing recatngles and finallizing process of developemnt

--- drawing_py_squares_by_generation_code/draw_some_cubes.py
+++ drawing_py_squares_by_generation_code/draw_some_cubes.py
@@ -1,16 +1,48 @@
 def get_image():
-    X_IMAGE_SIZE = 512
-    Y_IMAGE_SIZE = 512
-    
-    def draw_square(color_code: tuple):
-        pass
+    from PIL import Image, ImageDraw
+
+    def draw_square(pillow_image_draw: ImageDraw.Draw, cord_position: tuple, color_code: str):
+        pillow_image_draw.rectangle(cord_position, fill=color_code)
+
+    def execute_command_on_image(image: Image, command_list: list[list]):
+        color_codes = {0: "red", 1: "green", 2: "blue"}
+        pillow_image_draw = ImageDraw.Draw(image)
+        for rectangle_type_index in range(len(command_list)):
+            for rectangle_cords in command_list[rectangle_type_index]:
+                draw_square(pillow_image_draw, rectangle_cords, color_codes[rectangle_type_index])
 
     def process_string_command(command_text: str) -> list[list]:
-        return eval(command_text)    
+        source_command_list = [[], [], []]
+
+        def check_valid_command(command_list: list[list]) -> bool:
+            return type(command_list) == list and\
+                   len(command_list) == len(source_command_list) and\
+                   all([type(section) == list for section in command_list])
+        
+        try:
+            command_list_for_check = eval(command_text)
+            if check_valid_command(command_list_for_check):
+                return command_list_for_check
+            else:
+                raise NameError("Invalid command")
+        except NameError:
+            pass
+                
+        return source_command_list     
 
     def main():
+        X_IMAGE_SIZE = 512
+        Y_IMAGE_SIZE = 512
+
+        COLOR_CODE = "RGB"
+
+        BACKGROUND_COLOR = (255, 255, 255)
+
         commands_list = process_string_command(command_text=input())
-        
+        image = Image.new(COLOR_CODE, (X_IMAGE_SIZE, Y_IMAGE_SIZE), BACKGROUND_COLOR)
+        execute_command_on_image(image, commands_list)
+        image.show()
+        print(commands_list)    
 
     #starting the program
     main()
